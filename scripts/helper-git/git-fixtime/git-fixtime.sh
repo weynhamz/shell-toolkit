0b4baaf332d548d79b1a2b223b48015623ae82a9
Techlive Zheng <techlivezheng@gmail.com>
2012-06-11 17:07:06 +0800

    git-fixtime.sh: Add help message and additional infomation.

--- scripts/helper-git/git-fixtime/git-fixtime.sh
+++ scripts/helper-git/git-fixtime/git-fixtime.sh
@@ -2,8 +2,88 @@
 #
 # vim: set tabstop=4 shiftwidth=4 expandtab autoindent:
 #
+#==============================================================================
+#
+# FILE: git-fixtime.sh
+#
+# AUTHOR: Techlive Zheng <techlivezheng@gmail.com>
+#
+# Fix the time mass caused by the git rebase or other git operations.
+# Set the author date and/or committer date of certain commit to specific time.
+#
+# EXAMPLEA:
+#
+#   * Change the committer date of all the commits to its author date.
+#
+#           git-fixtime.sh -f
+#
+#   * Change the committer date of the given commit[s] to its author date.
+#
+#           git-fixtime.sh -f HEAD                  # Any valid ref in git
+#           git-fixtime.sh -f a203382
+#           git-fixtime.sh -f a203382 b73215f
+#           git-fixtime.sh -f -r a203382~..b73215f  # All the commits between
+#                                                     a203382 and b73215f, the
+#                                                     appended ~ of a203382 is
+#                                                     necessary.
+#
+#   * Change the author date of the given commit[s] to a given date.
+#
+#           git-fixtime.sh -a -d "Mon, 07 Aug 2006 12:34:56 -0600" a203382
+#
+#   * Change the author date as well as committer date of the given commit[s]
+#     to a given date.
+#
+#           git-fixtime.sh -a -c -d "Mon, 07 Aug 2006 12:34:56 -0600" a203382
+#
+#       Equals to
+#
+#           git-fixtime.sh -f -a -d "Mon, 07 Aug 2006 12:34:56 -0600" a203382
+#
+#   * The following will do nothing, it is nouse.
+#
+#           git-fixtime.sh -c -f -d "Mon, 07 Aug 2006 12:34:56 -0600" a203382
+#
+#   * Generate the date by randomly increasing the given date. The increasing
+#     range is 3 minitus.
+#
+#           git-fixtime.sh -a -f -d "Mon, 07 Aug 2006 12:34:56 -0600" -i -r a203382~..b73215f
+#
+#   -t flag could be used to examing the command before an actural performing.
+#
+#   If bad things happened, `git reflog` could help to get the last ref back.
+#
+#   Performing on a temporary branch is a good choice. If everything goes fine,
+#   then hard reset the HEAD of the original branch to the temporary one.
+#
+#==============================================================================
 
-while getopts :acd:firt opt
+showhelp() {
+   cat << EOF
+
+USAGE:
+
+    git-fixtime.sh [ -t ] [ -h ] [ -f ] [ ( -a | -c ) -d DATE [ -i ] ] COMMIT1 COMMIT2 ...
+    git-fixtime.sh [ -t ] [ -h ] [ -f ] [ ( -a | -c ) -d DATE [ -i ] ] -r COMMIT1..COMMIT2
+
+OPTIONS:
+
+        -a Change author date to the given date.
+        -c Change committer date to the given date.
+        -d RFC 2822 format string. Example: Mon, 07 Aug 2006 12:34:56 -0600
+        -f Set commiter date same as author date.
+        -r The given argument is a commit range. Example: a203382..b73215f
+        -t Debug mode, output the command to be executed.
+        -i Generate the date by randomly increasing the given date. The increasing range is 3 minites.
+        -h Show help message.
+
+    For more example, please read the source.
+
+EOF
+exit 0
+}
+
+while getopts :acd:fhirt opt
 do
     case $opt in
     'd')    date=$(LC_ALL=C date -R --date="$OPTARG")
@@ -20,6 +100,8 @@ do
     't')    debug="TRUE"
             ;;
     'i')    increase="TRUE"
+            ;;
+    'h')    showhelp
             ;;
       ?)    echo "Invalid Arg"
             exit 1
