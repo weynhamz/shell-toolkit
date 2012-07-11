314c02dda390de2b7ce49c537103d67e09e39f87
Techlive Zheng <techlivezheng@gmail.com>
2012-07-11 10:02:27 +0800

    makepkg.sh: Remove old files in building dir.

--- scripts/helper-al/makepkg/makepkg.sh
+++ scripts/helper-al/makepkg/makepkg.sh
@@ -2,6 +2,7 @@
 
 [ -f PKGBUILD ] || exit 1
 
+WORKSPACE=$(pwd)
 BUILDROOT=/home/abs/builds
 
 source ./PKGBUILD
@@ -10,8 +11,12 @@ source ./PKGBUILD
 
 mkdir -p $BUILDDIR
 
-cp * $BUILDDIR
-
 cd $BUILDDIR
+
+# Remove old files
+touch HOLDER && \
+rm $(ls -1 --color=none | grep -v -e '^pkg$' -e '^src$' -e '^.*\.pkg\.tar\.xz$' -e '^.*\.src\.tar\.gz$')
+
+cp $WORKSPACE/* .
 
 makepkg $@
