efa30dd12c539bff5db5abbfa8a14f81c2a5acb1
Techlive Zheng <techlivezheng@gmail.com>
2012-08-20 06:39:18 +0800

    git-init-by-mtime.sh: Using temporary directory.

--- scripts/helper-git/git-init-by-mtime/git-init-by-mtime.sh
+++ scripts/helper-git/git-init-by-mtime/git-init-by-mtime.sh
@@ -4,7 +4,7 @@ LC_ALL=C
 
 dest=$1
 
-rm -f files.* gitlog
+workspace=`mktemp -d`
 
 rm -rf $dest.new && cp -rf $dest $dest.new
 
@@ -13,7 +13,7 @@ do
     # Only match date here, ignore time
     date=`stat $file | grep "Modify:" | cut -n -d: -f2- | sed 's/[0-9]\{2\}\(\.[0-9]\{9\} +[0-9]\{4\}\)/00\1/g'`
     file=${file#$dest/}
-    [ -f $dest.new/.gitignore ] && grep -q $file $dest.new/.gitignore || echo $file >> files.`date -d "$date" '+%s'`
+    [ -f $dest.new/.gitignore ] && grep -q $file $dest.new/.gitignore || echo $file >> $workspace/files.`date -d "$date" '+%s'`
 done
 
 for file in `find $dest`
@@ -26,13 +26,13 @@ cd $dest.new
 
 git init
 
-for file in `ls -1 ../files.* | sort`
+for file in `ls -1 $workspace/files.* | sort`
 do
-    date=${file#../files.}
+    date=${file#$workspace/files.}
     git add `cat $file`
     git commit -m "`date -R --date="@$date"`" 2>&1 1>/dev/null
 done
 
-git log --pretty=oneline > ../gitlog
+git log --pretty=oneline > $workspace/gitlog
 
-git filter-branch -f --env-filter 'export GIT_AUTHOR_DATE=`grep $GIT_COMMIT ../../../gitlog | cut -d" " -f2-` && export GIT_COMMITTER_DATE=$GIT_AUTHOR_DATE || cat' -- --all
+git filter-branch -f --env-filter 'export GIT_AUTHOR_DATE=`grep $GIT_COMMIT '$workspace'/gitlog | cut -d" " -f2-` && export GIT_COMMITTER_DATE=$GIT_AUTHOR_DATE || cat' -- --all
