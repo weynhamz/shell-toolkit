ed3b1e9acce7d46347a3ea5005870fdff7806f5a
Techlive Zheng <techlivezheng@gmail.com>
2013-10-03 22:34:58 +0800

    git-subtree-extract.sh: Clone all remote branches as local

--- scripts/helper-git/git-subtree-extract/git-subtree-extract.sh
+++ scripts/helper-git/git-subtree-extract/git-subtree-extract.sh
@@ -6,6 +6,10 @@ git clone --no-hardlinks $1 $2
 
 cd $2
 
+for branch in `git branch -a | grep remotes | grep -v HEAD | grep -v master`; do
+    git branch --track ${branch#remotes/origin/} $branch
+done
+
 git filter-branch --subdirectory-filter $2 --prune-empty --tag-name-filter cat -- --all
 
 git reset --hard
