94e7359dc0a7c7ef64a4b4a9830fd7dfc9e2eb3c
Techlive Zheng <techlivezheng@gmail.com>
2013-10-26 01:48:14 +0800

    git-recursive-mv.sh: Prune empty commits after filtering

--- scripts/helper-git/git-recursive-mv/git-recursive-mv.sh
+++ scripts/helper-git/git-recursive-mv/git-recursive-mv.sh
@@ -84,6 +84,6 @@ fi
 
 [ -n "$branch" ] && range="$branch"
 
-cmd='git filter-branch -f --index-filter '\''git ls-files -s | sed "'$sedexp'" | GIT_INDEX_FILE=$GIT_INDEX_FILE.new git update-index --index-info && mv $GIT_INDEX_FILE.new $GIT_INDEX_FILE'\'' '$range
+cmd='git filter-branch -f --prune-empty --index-filter '\''git ls-files -s | sed "'$sedexp'" | GIT_INDEX_FILE=$GIT_INDEX_FILE.new git update-index --index-info && mv $GIT_INDEX_FILE.new $GIT_INDEX_FILE'\'' '$range
 
 eval "$cmd"
